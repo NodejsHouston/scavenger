52bfa8e22fdb5cc22c8334915730c4df2bacc7ab
sdossettswift <sdossettswift@gmail.com>
2016-06-07 20:59:41 -0500

    fleshed out elements for login page

--- src/client/components/login.js
+++ src/client/components/login.js
@@ -3,10 +3,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 export default class Login extends React.Component {
+  onSubmit(e) {
+    e.preventDefault();
+    console.log('hi');
+  }
+  onForgot(e) {
+    e.preventDefault();
+    console.log('hi, forgetful!');
+  }
+  onChange(e){
+    console.log("We will remember you!");
+  }
+
+
   render() {
     return (
       <div>
         <h1>Login</h1>
+        <form onSubmit={(e) => this.onSubmit(e) }>
+          <input placeholder="username"/>
+          <input placeholder="password"/>
+          <button type="submit">Login</button>
+        </form>
+        <input type="checkbox" onChange={(e)=> this.onChange(e)}/>Remember Me
+        <button onClick={(e) => this.onForgot(e)}>Forgot Your Password?</button>
+
       </div>
     );
   }
