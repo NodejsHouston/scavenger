c525ea2d0b8d068f91e42888e182a4c2912e0ce4
Matthew Simo <matthew.a.simo@gmail.com>
2016-06-07 20:09:19 -0500

    base structure for login & sign up routes/components

--- src/client/routes.js
+++ src/client/routes.js
@@ -4,6 +4,8 @@ import { Route } from 'react-router'
 import { Router, browserHistory } from 'react-router'
 
 import Home from "./components/home";
+import Login from "./components/login";
+import SignUp from "./components/sign-up";
 import About from "./components/about";
 import NoMatch from "./components/no-match";
 
@@ -13,6 +15,10 @@ export default class Routes extends React.Component {
       <Router history={browserHistory}>
         <Route path="/" component={Home}>
           <Route path="/about" component={About}>
+          </Route>
+          <Route path="/login" component={Login}>
+          </Route>
+          <Route path="/sign-up" component={SignUp}>
           </Route>
         </Route>
         <Route path="*" component={NoMatch}/>
